7c3ebf7a8d0c3935b2f1cd57cb675d31fd6eb6e5
VCM admin account <vcm@vcm-3502.vm.duke.edu>
2018-03-07 21:47:16 -0500

    All get/post requests are working.

--- client.py
+++ client.py
@@ -1,5 +1,12 @@
-from flask import Flask, jsonify, request
 import requests
-# import requests
 r = requests.get("http://vcm-3502.vm.duke.edu:5000/")
-print(r)
+print(r.text)
+
+getname = requests.get("http://vcm-3502.vm.duke.edu:5000/name")
+print(getname.json())
+
+hello_name = requests.get("http://vcm-3502.vm.duke.edu:5000/hello/Marianne")
+print(hello_name.json())
+
+distance = requests.post("http://vcm-3502.vm.duke.edu:5000/distance",json={"a": [0, 5], "b": [4, 0]})
+print(distance.json())
